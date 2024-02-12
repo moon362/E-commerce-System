cca2bb21ed0628d4104b646fd0ee2cc8dd1285e3
rasel <jucse28.367@gmail.com>
2024-02-12 22:08:10 +0600

    initial commit

---
+++ backend/api/views/user_views.py
@@ -0,0 +1,28 @@
+
+from backend.api.serializers import UserSerializerWithToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+       
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k,v in serializer.items():
+            data[k] =v
+
+        return data
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['message'] = "Hello Proshop"
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
