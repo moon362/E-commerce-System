9628b41c57860d6473b0d46ecd8c69ead9669249
Umme Faria Moon <ummefariajucse28.362@gmail.com>
2024-02-13 00:30:08 +0600

    api views

--- backend/api/views/order_views.py
+++ backend/api/views/order_views.py
@@ -13,6 +13,70 @@ from rest_framework.serializers import Serializer
 from api.models import *
 from api.serializers import ProductSerializer, OrderSerializer
 
+#views 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+    print(data)
+    orderItems = data['orderItems']
+
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail': 'No Order Items', "status": status.HTTP_400_BAD_REQUEST})
+    else:
+        # (1) Create Order
+        order = Order.objects.create(
+            user=user,
+            paymentMethod=data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            shippingPrice=data['shippingPrice'],
+            totalPrice=data['totalPrice'],
+        )
+
+        # (2) Create Shipping Address
+
+        shipping = ShippingAddress.objects.create(
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postalCode=data['shippingAddress']['postalCode'],
+            country=data['shippingAddress']['country'],
+        )
+
+        # (3) Create order items
+
+        for i in orderItems:
+            # product = Product.objects.get(_id=i['product'])
+            product = Product.objects.get(_id=i['_id'])
+
+            item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=product.image.url,
+            )
+
+            # (4) Update Stock
+                        # product.countInStock -= int(item["qty"]  # Convert item["qty"] to an integer
+
+            product.countInStock -= int(item.qty)
+            product.save()
+
+        serializer = OrderSerializer(order, many=False)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getMyOrders(request):
+    user = request.user
+    orders = user.order_set.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
 
 
 
