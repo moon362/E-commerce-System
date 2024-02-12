c608bc3c25ecaf316fb69f58f5f327a999eb737a
rasel <jucse28.367@gmail.com>
2024-02-12 22:18:04 +0600

    initial commit

--- backend/api/views/user_views.py
+++ backend/api/views/user_views.py
@@ -2,6 +2,12 @@
 from backend.api.serializers import UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+# Rest Framework Import
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated 
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
@@ -26,3 +32,15 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
+# SHOP API
+@api_view(['GET'])
+def getRoutes(request):
+    routes =[
+        '/api/products/',
+        '/api/products/<id>',
+        '/api/users',
+        '/api/users/register',
+        '/api/users/login',
+        '/api/users/profile',
+    ]
+    return Response(routes)
