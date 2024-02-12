f45181720452b5c69f2cb896d2621e28f7ead267
Umme Faria Moon <ummefariajucse28.362@gmail.com>
2024-02-13 00:27:15 +0600

    framework import and api model import

--- backend/api/views/order_views.py
+++ backend/api/views/order_views.py
@@ -4,3 +4,15 @@ from datetime import datetime
 
 from rest_framework import status
 
+#import framework
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+
+from api.models import *
+from api.serializers import ProductSerializer, OrderSerializer
+
+
+
+
