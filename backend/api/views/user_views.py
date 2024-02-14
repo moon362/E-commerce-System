3efeb07d609f15825a1b063401650c40445b5d52
rasel <jucse28.367@gmail.com>
2024-02-15 00:22:18 +0600

    updated user view with coding standard

--- backend/api/views/user_views.py
+++ backend/api/views/user_views.py
@@ -1,27 +1,25 @@
-
-
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 from backend.api.serializers import UserSerializer, UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
-# Rest Framework Import
-from rest_framework.decorators import api_view,permission_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated 
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
        
         serializer = UserSerializerWithToken(self.user).data
 
-        for k,v in serializer.items():
-            data[k] =v
+        for k, v in serializer.items():
+            data[k] = v
 
         return data
+
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
@@ -38,7 +36,7 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 # SHOP API
 @api_view(['GET'])
-def getRoutes(request):
+def get_routes(request):
     routes =[
         '/api/products/',
         '/api/products/<id>',
@@ -48,44 +46,47 @@ def getRoutes(request):
         '/api/users/profile',
     ]
     return Response(routes)
+
 @api_view(['POST'])
-def registerUser(request):
+def register_user(request):
     data = request.data
     try:
         user = User.objects.create(
-            first_name = data['name'],
-            username = data['email'],
-            password = make_password(data['password']),
+            first_name=data['name'],
+            username=data['email'],
+            password=make_password(data['password']),
         )
 
-        serializer = UserSerializerWithToken(user,many=False)
+        serializer = UserSerializerWithToken(user, many=False)
         return Response(serializer.data)
     
     except:
         message = {"detail":"User with this email is already registered"}
-        return Response(message,status=status.HTTP_400_BAD_REQUEST)
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def getUserProfile(request):
-    user =request.user 
-    serializer = UserSerializer(user,many = False)
+def get_user_profile(request):
+    user = request.user 
+    serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
+
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
-def updateUserProfile(request):
-    user =request.user 
-    serializer = UserSerializerWithToken(user,many = False)
+def update_user_profile(request):
+    user = request.user 
+    serializer = UserSerializerWithToken(user, many=False)
     data = request.data
     user.first_name = data['name']
     user.username = data['email']
     user.email = data['email']
-    if data['password'] !="":
-        user.password= make_password(data['password'])
+    if data['password'] != "":
+        user.password = make_password(data['password'])
     user.save()
     return Response(serializer.data)
+
 @api_view(['DELETE'])
-def deleteUser(request,pk):
-    userForDeletion = User.objects.get(id=pk)
-    userForDeletion.delete()
+def delete_user(request, pk):
+    user_for_deletion = User.objects.get(id=pk)
+    user_for_deletion.delete()
     return Response("User was deleted")
