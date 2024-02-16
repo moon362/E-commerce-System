bba2312b11270478c7edca2fb35447b869a2bc94
shreyanagriya <nag.stu2018@juniv.edu>
2024-02-16 00:03:32 -0800

    documentation added for components searchbox and product carousel

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -6,14 +6,20 @@ import Loader from "./Loader";
 import Message from "./Message";
 import { fetchTopRatedProducts } from "../redux/slices/productSlice";
 
+/**
+ * Functional component representing a carousel of top-rated products.
+ * @component
+ * @returns {JSX.Element} ProductCarousel component
+ */
 function ProductCarousel() {
   const dispatch = useDispatch();
+  // Selecting top-rated products state from Redux store
   const topRatedProducts = useSelector(
     (state) => state.product.topRatedProducts
   );
   const { error, loading, products } = topRatedProducts;
-  console.log(topRatedProducts);
 
+  // Fetch top-rated products when the component mounts
   useEffect(() => {
     dispatch(fetchTopRatedProducts());
   }, [dispatch]);
@@ -23,6 +29,7 @@ function ProductCarousel() {
   ) : error ? (
     <Message variant="danger">{error}</Message>
   ) : (
+    // Display carousel with top-rated products
     <Carousel
       style={{ height: "300px" }}
       pause="hover"
@@ -30,13 +37,16 @@ function ProductCarousel() {
       interval={5000}
     >
       {products.map((product) => (
+        // Carousel item for each top-rated product
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
+            {/* Product image */}
             <Image
               src={product.image}
               style={{ height: "250px", width: "250px" }}
               alt={product.name}
             />
+            {/* Product caption */}
             <Carousel.Caption className="carousel-caption">
               <h4>
                 {product.name} (₹{product.price})
