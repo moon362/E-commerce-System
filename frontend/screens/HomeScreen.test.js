000a6ea87df8ac6ac9e437e112e45b14c07caf60
rasel <jucse28.367@gmail.com>
2024-02-23 00:10:40 +0600

    making some changes

--- frontend/screens/HomeScreen.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import HomeScreen from './HomeScreen';
-
-test('renders HomeScreen component', () => {
-  const { getByText } = render(<HomeScreen />);
-  const headerElement = getByText(/TOP-RATED PRODUCTS/i);
-  expect(headerElement).toBeInTheDocument();
-});
