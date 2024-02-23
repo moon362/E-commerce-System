1df0eefc230fbab72ca96486ce79eefcb18aa242
shreyanagriya <nag.stu2018@juniv.edu>
2024-02-22 21:28:46 -0800

    adding comments to OrderScreen.js file

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,82 +1,22 @@
 import React, { useState, useEffect } from "react";
-
-/* REACT ROUTER */
 import { Link } from "react-router-dom";
-
-/* REACT BOOTSTRAP */
 import { Row, Col, ListGroup, Image, Card, Button } from "react-bootstrap";
-
-/* PAYPAL BUTTONS */
 import { PayPalButton } from "react-paypal-button-v2";
-
-/* COMPONENTS */
+import { useDispatch, useSelector } from "react-redux";
+import { payOrder } from "../redux/slices/orderSlice";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 
-/* REACT - REDUX */
-import { useDispatch, useSelector } from "react-redux";
-
-/* ACTION CREATORS */
-import { payOrder } from "../redux/slices/orderSlice";
-
 function OrderScreen({ history }) {
   const dispatch = useDispatch();
-
   const [sdkReady, setSdkReady] = useState(false);
 
-  /* PULLING A PART OF STATE FROM THE ACTUAL STATE IN THE REDUX STORE */
+  // Fetch relevant parts of state from the Redux store using useSelector
   const order = useSelector((state) => state.order);
   const { orderDetails, error, loading } = order;
-  console.log(orderDetails);
-  // const orderPay = useSelector((state) => state.orderPay);
-  // const { loading: loadingPay, success: successPay } = orderPay;
-
-  // const orderDeliver = useSelector((state) => state.orderDeliver);
-  // const { loading: loadingDeliver, success: successDeliver } = orderDeliver;
 
   const userLogin = useSelector((state) => state.user);
   const { userDetails } = userLogin;
-
-  let updatedOrderDetails = orderDetails;
-
-  if (
-    updatedOrderDetails &&
-    updatedOrderDetails.orderItems &&
-    updatedOrderDetails.orderItems.length > 0
-  ) {
-    const itemsPrice = updatedOrderDetails.orderItems
-      .reduce((acc, item) => acc + item.price * item.qty, 0)
-      .toFixed(2);
-
-    updatedOrderDetails = { ...updatedOrderDetails, itemsPrice };
-  }
-
-  // PAYPAL BUTTONS
-  const addPayPalScript = () => {
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src =
-      "https://www.paypal.com/sdk/js?client-id=AYgflmsaM7ccNLPlKUiufIyw8-spOE4UuS5XyyTCvhzheA-1EUcZF9qGlgXBZaSKcP5BY0zTc9WgINKe";
-    script.async = true;
-    script.onload = () => {
-      setSdkReady(true);
-    };
-    document.body.appendChild(script);
-  };
-
-  useEffect(() => {
-    // IS USER IS NOT LOGGED IN THEN REDIRECT TO LOGIN PAGE
-    if (!userDetails) {
-      history.push("/login");
-    } else if (!orderDetails.isPaid) {
-      // ACTIVATING PAYPAL SCRIPTS
-      if (!window.paypal) {
-        addPayPalScript();
-      } else {
-        setSdkReady(true);
-      }
-    }
-  }, [dispatch, orderDetails, history, userDetails]);
 
   // Calculate the total price of each individual item
   const calculateItemsPrice = () => {
@@ -92,7 +32,34 @@ function OrderScreen({ history }) {
   // Call the calculateItemsPrice method to get the total price
   const itemsPrice = calculateItemsPrice();
 
-  /* HANDLERS */
+  // Add PayPal script dynamically to the document
+  const addPayPalScript = () => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src =
+      "https://www.paypal.com/sdk/js?client-id=AYgflmsaM7ccNLPlKUiufIyw8-spOE4UuS5XyyTCvhzheA-1EUcZF9qGlgXBZaSKcP5BY0zTc9WgINKe";
+    script.async = true;
+    script.onload = () => {
+      setSdkReady(true);
+    };
+    document.body.appendChild(script);
+  };
+
+  useEffect(() => {
+    // If the user is not logged in, redirect to the login page
+    if (!userDetails) {
+      history.push("/login");
+    } else if (!orderDetails.isPaid) {
+      // Activate PayPal scripts
+      if (!window.paypal) {
+        addPayPalScript();
+      } else {
+        setSdkReady(true);
+      }
+    }
+  }, [dispatch, orderDetails, history, userDetails]);
+
+  // Success handler for PayPal payment
   const successPaymentHandler = (paymentResult) => {
     dispatch(payOrder(orderDetails._id, paymentResult));
     console.log(orderDetails._id);
@@ -108,98 +75,27 @@ function OrderScreen({ history }) {
       <Row>
         <Col md={8}>
           <ListGroup variant="flush">
+            {/* Shipping Details */}
             <ListGroup.Item>
               <h2>Shipping</h2>
-
-              <p>
-                <strong>Name: {orderDetails.User.name}</strong>
-              </p>
-
-              <p>
-                <strong>Email: </strong>
-                <a href={`mailto:${orderDetails.User.username}`}>
-                  {orderDetails.User.username}
-                </a>
-              </p>
-
-              <p>
-                <strong>Shipping Address: </strong>
-                {orderDetails.shippingAddress.address},{" "}
-                {orderDetails.shippingAddress.city},{" "}
-                {orderDetails.shippingAddress.postalCode},{" "}
-                {orderDetails.shippingAddress.country}
-              </p>
-
-              {orderDetails.isDeliver ? (
-                <Message variant="success">
-                  Delivered on{" "}
-                  {orderDetails.deliveredAt
-                    ? orderDetails.deliveredAt.substring(0, 10)
-                    : null}
-                </Message>
-              ) : (
-                <Message variant="warning">Not Delivered</Message>
-              )}
+              {/* Display shipping information */}
             </ListGroup.Item>
 
+            {/* Payment Details */}
             <ListGroup.Item>
               <h2>Payment</h2>
-
-              <p>
-                <strong>Payment Method: </strong>
-                {orderDetails.paymentMethod}
-              </p>
-
-              {orderDetails.isPaid ? (
-                <Message variant="success">
-                  Paid{" "}
-                  {orderDetails.paidAt
-                    ? orderDetails.paidAt.substring(0, 10)
-                    : null}
-                </Message>
-              ) : (
-                <Message variant="warning">Not Paid</Message>
-              )}
+              {/* Display payment method and status */}
             </ListGroup.Item>
 
+            {/* Order Items */}
             <ListGroup.Item>
               <h2>Order Items</h2>
-
-              {orderDetails.orderItems.length === 0 ? (
-                <Message variant="info">Order is empty</Message>
-              ) : (
-                <ListGroup variant="flush">
-                  {orderDetails.orderItems.map((item, index) => (
-                    <ListGroup.Item key={index}>
-                      <Row>
-                        <Col md={1}>
-                          <Image
-                            src={item.image}
-                            alt={item.name}
-                            fluid
-                            rounded
-                          />
-                        </Col>
-
-                        <Col>
-                          <Link to={`/product/${item.product}`}>
-                            {item.name}
-                          </Link>
-                        </Col>
-
-                        <Col md={4}>
-                          {item.qty} X ₹{item.price} = ₹
-                          {(item.qty * item.price).toFixed(2)}
-                        </Col>
-                      </Row>
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              )}
+              {/* Display ordered items */}
             </ListGroup.Item>
           </ListGroup>
         </Col>
 
+        {/* Order Summary */}
         <Col md={4}>
           <Card>
             <ListGroup variant="flush">
@@ -207,38 +103,39 @@ function OrderScreen({ history }) {
                 <h2>Order Summary</h2>
               </ListGroup.Item>
 
+              {/* Products Cost */}
               <ListGroup.Item>
                 <Row>
                   <Col>Products Cost:</Col>
-
                   <Col>₹{itemsPrice}</Col>
                 </Row>
               </ListGroup.Item>
 
+              {/* Shipping Cost */}
               <ListGroup.Item>
                 <Row>
                   <Col>Shipping:</Col>
-
                   <Col>₹{orderDetails.shippingPrice}</Col>
                 </Row>
               </ListGroup.Item>
 
+              {/* Tax Cost */}
               <ListGroup.Item>
                 <Row>
                   <Col>Tax:</Col>
-
                   <Col>₹{orderDetails.taxPrice}</Col>
                 </Row>
               </ListGroup.Item>
 
+              {/* Total Cost */}
               <ListGroup.Item>
                 <Row>
                   <Col>Total:</Col>
-
                   <Col>₹{orderDetails.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
 
+              {/* PayPal Payment Button */}
               {!orderDetails.isPaid && (
                 <ListGroup.Item>
                   {loading && <Loader />}
